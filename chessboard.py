ba668ef87d04ab29ceb1c3d959d7dc41d7dd259d
Tom Gong <trgjtk@gmail.com>
2024-03-18 17:26:46 -0500

    chessboard class

---
+++ chessboard.py
@@ -0,0 +1,7 @@
+import numpy as np
+class ChessBoard():
+    def __init__(self):
+        self.pieces = np.zeros((2, 6), dtype = np.uint64)
+        self.color_combined = np.zeros(2, dtype=np.uint64)
+        self.all_combined = np.zeros(2, dtype=np.uint64)
+        self.color = Color.WHITE
