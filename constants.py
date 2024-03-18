b0791a1d7abcfb135ae02df6c4bcf45567af6d58
/ <thomasgong@s-MacBook-Air.local>
2024-03-18 17:02:27 -0500

    updating constants

--- constants.py
+++ constants.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+from enum import IntEnum
 # Define hexadecimal representations for each row
 ROW_1 = 0xFF
 ROW_2 = 0xFF00
@@ -58,3 +58,45 @@ def msb_bitscan(bb):
     bb |= bb >> np.uint8(16)
     bb |= bb >> np.uint8(32)
     return msb_lookup[(bb * debruijn) >> np.uint8(58)]
+
+class Piece(IntEnum):
+    PAWN = 0
+    KNIGHT = 1
+    BISHOP = 2
+    ROOK = 3
+    QUEEN = 4
+    KING = 5
+
+    def to_char(self):
+        if self == Piece.PAWN:
+            return 'p'
+        elif self == Piece.KNIGHT:
+            return 'n'
+        elif self == Piece.BISHOP:
+            return 'b'
+        elif self == Piece.ROOK:
+            return 'r'
+        elif self == Piece.QUEEN:
+            return 'q'
+        elif self == Piece.KING:
+            return 'k'
+
+class Rank(IntEnum):
+    ONE = 0
+    TWO = 1
+    THREE = 2
+    FOUR = 3
+    FIVE = 4
+    SIX = 5
+    SEVEN = 6
+    EIGHT = 7
+
+class File(IntEnum):
+    A = 0
+    B = 1
+    C = 2
+    D = 3
+    E = 4
+    F = 5
+    G = 6
+    H = 7
