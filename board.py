9dfcdec7a77789e733c1e9917553d295bf0d99b7
Tom Gong <trgjtk@gmail.com>
2024-03-19 17:27:06 -0500

    changes to chessboard

--- board.py
+++ board.py
@@ -1,35 +1,77 @@
 import numpy as np
-import constants
-class Board():
-    def __init__(self):
-        self.white_rook_bitboard = np.uint64(0)
-        self.white_bishop_bitboard = np.uint64(0)
-        self.white_king_bitboard = np.uint64(0)
-        self.white_pawn_bitboard = np.uint64(0)
-        self.white_knight_bitboard = np.uint64(0)
-        self.white_queen_bitboard = np.uint64(0)
+from constants import Rank, File
+from square import Square
+EMPTY_BB = np.uint64(0)
+#lsb (least significant bit) msb (most significant bit) trailing zero count trick
+debruijn = np.uint64(0x03f79d71b4cb0a89)
 
-        self.black_rook_bitboard = np.uint64(0)
-        self.black_bishop_bitboard = np.uint64(0)
-        self.black_king_bitboard = np.uint64(0)
-        self.black_pawn_bitboard = np.uint64(0)
-        self.black_knight_bitboard = np.uint64(0)
-        self.black_queen_bitboard = np.uint64(0) 
+lsb_lookup = np.array(
+        [ 0,  1, 48,  2, 57, 49, 28,  3,
+         61, 58, 50, 42, 38, 29, 17,  4,
+         62, 55, 59, 36, 53, 51, 43, 22,
+         45, 39, 33, 30, 24, 18, 12,  5,
+         63, 47, 56, 27, 60, 41, 37, 16,
+         54, 35, 52, 21, 44, 32, 23, 11,
+         46, 26, 40, 15, 34, 20, 31, 10,
+         25, 14, 19,  9, 13,  8,  7,  6],
+        dtype=np.uint8)
 
-    @property
-    def white_pieces_bitboard(self):
-        return self.white_rook_bitboard | self.white_bishop_bitboard | self.white_king_bitboard | self.white_pawn_bitboard | self.white_knight_bitboard | self.white_queen_bitboard
-    @property
-    def black_pieces_bitboard(self):
-        return self.black_rook_bitboard | self.black_bishop_bitboard | self.black_king_bitboard | self.black_pawn_bitboard | self.black_knight_bitboard | self.black_queen_bitboard
+msb_lookup = np.array(
+        [ 0, 47,  1, 56, 48, 27,  2, 60,
+         57, 49, 41, 37, 28, 16,  3, 61,
+         54, 58, 35, 52, 50, 42, 21, 44,
+         38, 32, 29, 23, 17, 11,  4, 62,
+         46, 55, 26, 59, 40, 36, 15, 53,
+         34, 51, 20, 43, 31, 22, 10, 45,
+         25, 39, 14, 33, 19, 30,  9, 24,
+         13, 18,  8, 12,  7,  6,  5, 63],
+        dtype=np.uint8)
 
-    @property
-    def occupied_squares_bitboard(self):
-        return self.white_pieces_bitboard | self.black_pieces_bitboard
+def lsb_bitscan(bb):
+    return lsb_lookup[((bb & -bb) * debruijn) >> np.uint8(58)]
 
-    @property
-    def empty_squares_bitboard(self):
-        return ~self.occupied_squares_bitboard
+def msb_bitscan(bb):
+    bb |= bb >> np.uint8(1)
+    bb |= bb >> np.uint8(2)
+    bb |= bb >> np.uint8(4)
+    bb |= bb >> np.uint8(8)
+    bb |= bb >> np.uint8(16)
+    bb |= bb >> np.uint8(32)
+    return msb_lookup[(bb * debruijn) >> np.uint8(58)]
+
+def occupied_squares(bb):
+    while bb != EMPTY_BB:
+        lsb_square = Square(lsb_bitscan(bb))
+        yield lsb_square
+        bb ^= lsb_square.to_bitboard()
+
+def pop_count(bb):
+    count = np.uint8(0)
+    while bb != EMPTY_BB:
+        count += np.uint8(1)
+        bb &= bb - np.uint64(1)
+    return count
+
+def is_set(bb, sq):
+    return (sq.to_bitboard() & bb) != EMPTY_BB
+
+def to_str(bb):
+    bb_str = []
+    for r in reversed(Rank):
+        for f in File:
+            sq = Square.from_position(r, f)
+            if is_set(bb, sq):
+                bb_str.append('1')
+            else:
+                bb_str.append('.')
+        bb_str.append('\n')
+    return ''.join(bb_str)
+
+def clear_square(bb, sq):
+    return (~sq.to_bitboard()) & bb
+
+def set_square(bb, sq):
+    return sq.to_bitboard() | bb
     
 
 
--- constants.py
+++ constants.py
@@ -1,44 +1,8 @@
 import numpy as np
 from enum import IntEnum
 from square import Square
+import board
 
-
-#lsb (least significant bit) msb (most significant bit) trailing zero count trick
-debruijn = np.uint64(0x03f79d71b4cb0a89)
-
-lsb_lookup = np.array(
-        [ 0,  1, 48,  2, 57, 49, 28,  3,
-         61, 58, 50, 42, 38, 29, 17,  4,
-         62, 55, 59, 36, 53, 51, 43, 22,
-         45, 39, 33, 30, 24, 18, 12,  5,
-         63, 47, 56, 27, 60, 41, 37, 16,
-         54, 35, 52, 21, 44, 32, 23, 11,
-         46, 26, 40, 15, 34, 20, 31, 10,
-         25, 14, 19,  9, 13,  8,  7,  6],
-        dtype=np.uint8)
-
-msb_lookup = np.array(
-        [ 0, 47,  1, 56, 48, 27,  2, 60,
-         57, 49, 41, 37, 28, 16,  3, 61,
-         54, 58, 35, 52, 50, 42, 21, 44,
-         38, 32, 29, 23, 17, 11,  4, 62,
-         46, 55, 26, 59, 40, 36, 15, 53,
-         34, 51, 20, 43, 31, 22, 10, 45,
-         25, 39, 14, 33, 19, 30,  9, 24,
-         13, 18,  8, 12,  7,  6,  5, 63],
-        dtype=np.uint8)
-
-def lsb_bitscan(bb):
-    return lsb_lookup[((bb & -bb) * debruijn) >> np.uint8(58)]
-
-def msb_bitscan(bb):
-    bb |= bb >> np.uint8(1)
-    bb |= bb >> np.uint8(2)
-    bb |= bb >> np.uint8(4)
-    bb |= bb >> np.uint8(8)
-    bb |= bb >> np.uint8(16)
-    bb |= bb >> np.uint8(32)
-    return msb_lookup[(bb * debruijn) >> np.uint8(58)]
 
 class Color(IntEnum):
     WHITE = 0
@@ -208,6 +172,7 @@ def gen_pawn_attacks(color, pos):
 
 PAWN_ATTACKS = np.fromiter((gen_pawn_attacks(color, pos) for color in Color for pos in range(64)), dtype = np.uint64, count = 128)
 PAWN_ATTACKS.shape = (2, 64)
+
 #first rank moves (double step) also check whether blocked or not
 def compute_first_rank_moves(i, occ):
     # i is square index from 0 to 8
