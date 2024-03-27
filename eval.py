2eeafbe259aac3cea611f60d2c821e3efdb55acf
/ <thomasgong@s-MacBook-Air.local>
2024-03-27 14:51:14 -0500

    eval mobility heuristic added

--- eval.py
+++ eval.py
@@ -1,4 +1,17 @@
 import chess
+def eval(board: chess.Board):
+    if len(board.legal_moves) == 0:
+        if board.is_checkmate():
+            return -10000
+        else:
+            return 0
+    #mobility heuristic
+    num_curr_moves = len(board.legal_moves)
+    board.turn = not board.turn
+    num_opp_moves = len(board.legal_moves)
+    board.turn = not board.turn
+    move_diff = num_curr_moves-num_opp_moves
 
-def eval(board):
-    return len(board.legal_moves)
+    
+        
+
