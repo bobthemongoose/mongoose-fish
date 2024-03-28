ad3da4d5f238736ef5a5f8bc753084f8263bd42d
/ <thomasgong@s-MacBook-Air.local>
2024-03-28 16:37:52 -0500

    updated constants

---
+++ constants.py
@@ -0,0 +1,3 @@
+import chess
+piece_types = [chess.PAWN, chess.KNIGHT, chess.KING, chess.BISHOP, chess.ROOK, chess.QUEEN]
+colors = [chess.WHITE, chess.BLACK]
--- eval.py
+++ eval.py
@@ -1,17 +1,21 @@
 import chess
+import constants
 def eval(board: chess.Board):
     if len(board.legal_moves) == 0:
         if board.is_checkmate():
             return -10000
         else:
             return 0
-    #mobility heuristic
+#mobility heuristic
+def mobility_diff(board: chess.Board):
     num_curr_moves = len(board.legal_moves)
     board.turn = not board.turn
     num_opp_moves = len(board.legal_moves)
     board.turn = not board.turn
-    move_diff = num_curr_moves-num_opp_moves
+    return num_curr_moves-num_opp_moves
 
-    
+def piece_importance(board: chess.Board):
+    for piece in chess.PieceType
+
         
 
