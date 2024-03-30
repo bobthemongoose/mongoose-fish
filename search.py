1fd086cb13893e62b9c216438cc81a18b474827f
Tom Gong <trgjtk@gmail.com>
2024-03-30 02:51:35 -0500

    finished first mini-max

--- search.py
+++ search.py
@@ -1,4 +1,21 @@
 import chess
-
-class Search:
-    
+import eval
+count = [0]
+def minimax(board: chess.Board, color: chess.Color, depth):
+    count[0] += 1
+    if depth == 0 or not board.legal_moves:
+        return eval.eval(board)
+    if color == chess.WHITE:
+        score = -10000
+        for move in board.legal_moves:
+            board.push(move)
+            score = max(score, minimax(board, chess.BLACK, depth - 1))
+            board.pop()
+        return score
+    else:
+        score = 10000
+        for move in board.legal_moves:
+            board.push(move)
+            score = min(score, minimax(board, chess.WHITE, depth - 1))
+            board.pop()
+        return score
