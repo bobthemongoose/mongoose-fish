fcc44f2139ea496f2da36359241d971591581259
Tom Gong <trgjtk@gmail.com>
2024-04-04 15:03:38 -0500

    transposition table not working

--- search.py
+++ search.py
@@ -28,16 +28,16 @@ transposition_table = TranspositionTable(zobrist_hash)  # Initialize the transpo
 
 
 def alphabeta(board: chess.Board, depth, alpha=-10000, beta=10000, key = None):
-    global transposition_table  # Assuming TranspositionTable is a global variable
-    global zobrist_hash
+    # global transposition_table  # Assuming TranspositionTable is a global variable
+    # global zobrist_hash
 
     # Check if the position is already stored in the transposition table
-    if not key:
-        key = zobrist_hash.hash(board)
-    table_entry = transposition_table.table.get(key)
-    if table_entry and table_entry[1] >= depth:
-        count[1] += 1
-        return table_entry[0], table_entry[2]  # Return the evaluation score and best move
+    # if not key:
+    #     key = zobrist_hash.hash(board)
+    # table_entry = transposition_table.table.get(key)
+    # if table_entry and table_entry[1] >= depth:
+    #     count[1] += 1
+    #     return table_entry[0], table_entry[2]  # Return the evaluation score and best move
 
     count[0] += 1
     
@@ -45,44 +45,46 @@ def alphabeta(board: chess.Board, depth, alpha=-10000, beta=10000, key = None):
     if depth == 0 or not board.legal_moves:
         # score = eval.eval(board)
         score = q_search(board, alpha, beta)
-        # if board.turn == chess.WHITE:
-        #     score = q_search(board, alpha, beta, key)
-        # else:
-        #     score = q_search(board, -beta, -alpha, key)
-        transposition_table.add_key(key, score, depth, None)  # Store the evaluation score
+        # transposition_table.add_key(key, score, depth, None)  # Store the evaluation score
         return score, None
 
     best_move = None
     max_score = -100000
     for move in board.legal_moves:
-        move_hash = zobrist_hash.update(key, move, board.piece_at(move.from_square))
+        # move_hash = zobrist_hash.update(key, move, board.piece_at(move.from_square))
         board.push(move)
-        score, _ = alphabeta(board, depth - 1, -beta, -alpha, move_hash)
+        # score, _ = alphabeta(board, depth - 1, -beta, -alpha, move_hash)
+        score, _ = alphabeta(board, depth - 1, -beta, -alpha)
         score = -score
         board.pop()
 
         # Update alpha and beta
             
         if score >= beta:
-            transposition_table.add_key(key, beta, depth, move)  # Store the evaluation score and best move
+            # transposition_table.add_key(key, score, depth, move)  # Store the evaluation score and best move
             return beta, move
         if score > max_score:
             best_move = move
             max_score = score
         alpha = max(score, alpha)
-    transposition_table.add_key(key, alpha, depth, best_move)  # Store the evaluation score and best move
+    # transposition_table.add_key(key, alpha, depth, best_move)  # Store the evaluation score and best move
     return alpha, best_move
 
-
+def translate_score(color: chess.Color, eval):
+    if color == chess.WHITE:
+        return eval
+    else:
+        return -eval
+    
 def q_search(board: chess.Board, alpha, beta, key = None):
-    global transposition_table
-    global zobrist_hash
-    if not key:
-        key = zobrist_hash.hash(board)
-    table_entry = transposition_table.table.get(key)
+    # global transposition_table
+    # global zobrist_hash
+    # if not key:
+    #     key = zobrist_hash.hash(board)
+    # table_entry = transposition_table.table.get(key)
 
-    if table_entry and table_entry[1] == 0:
-        return table_entry[0]
+    # if table_entry and table_entry[1] == 0:
+    #     return table_entry[0]
     stand_pat = eval.eval(board)
     if stand_pat >= beta:
         return stand_pat
@@ -90,9 +92,10 @@ def q_search(board: chess.Board, alpha, beta, key = None):
         alpha = stand_pat
     moves = board.generate_pseudo_legal_captures()
     for move in moves:
-        move_hash = zobrist_hash.update(key, move, board.piece_at(move.from_square))
+        # move_hash = zobrist_hash.update(key, move, board.piece_at(move.from_square))
         board.push(move)
-        score = -q_search(board, -beta, -alpha, move_hash)
+        # score = -q_search(board, -beta, -alpha, move_hash)
+        score = -q_search(board, -beta, -alpha)
         board.pop()
         if score > stand_pat:
             stand_pat = score
@@ -100,7 +103,7 @@ def q_search(board: chess.Board, alpha, beta, key = None):
                 alpha = score
                 if score >= beta:
                     break
-    transposition_table.add_key(key, stand_pat, 0, None)
+    # transposition_table.add_key(key, stand_pat, 0, None)
     return stand_pat
 
 def id_search(board: chess.Board, max_depth):
--- test.py
+++ test.py
@@ -5,17 +5,27 @@ import timeit
 
 # Define a function that wraps the alphabeta call
 def run_alphabeta():
-    fen = "rnbqkbnr/pppp1ppp/8/4p3/4P3/5N2/PPPP1PPP/RNBQKB1R b KQkq - 0 1"
+    fen = "rnb1kbnr/pppp1ppp/8/4p3/4P2q/5N2/PPPP1PPP/RNBQKB1R w KQkq - 2 3"
     board = chess.Board(fen)
     # result = eval.eval(board)
     # result = search.q_search(board, -10000, 10000)
-    result = search.id_search(board, 4)
+    result = search.alphabeta(board, 2)
     if board.turn != chess.WHITE:
         print(-result[0], result[1])
     else:
         print(result[0], result[1])
     # print(result)
 # Time the execution of the function
-execution_time = timeit.timeit(run_alphabeta, number=1)
-print("Execution time:", execution_time, "seconds")
+# execution_time = timeit.timeit(run_alphabeta, number=1)
+# print("Execution time:", execution_time, "seconds")
 
+fen = "rnbqkbnr/pppp1ppp/8/4p3/4P3/5N2/PPPP1PPP/RNBQKB1R b KQkq - 1 2"
+board = chess.Board(fen)
+result = search.alphabeta(board, 3)
+print(result)
+for move in board.legal_moves:
+    board.push(move)
+    print(move)
+    print(search.transposition_table.table[search.zobrist_hash.hash(board)])
+    board.pop()
+print(search.transposition_table.table[search.zobrist_hash.hash(board)])
